132cf155e874fab77ba21fe92cbabbf528301567
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-09-29 19:56:15 +0600

    data loaded from card

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import './Main.css';
 import Card from '../Card/Card';
+import Sidebar from '../Sidebar/Sidebar';
 
 
 const Main = () => {
+
+    const [cards, setCards] = useState([]);
+    
+    useEffect(() => {
+        fetch('cards.json')
+        .then(res => res.json())
+        .then(data => setCards(data))
+    }, [])
+
+
     return (
         <div className='container main-area'>
-            <h3><span><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon></span> Mild Exercise Club</h3>
-            <Card></Card>
+            <h2><span><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon></span> Mild Exercise Club</h2>
+
+            <div className="row">
+                <div className="col-md-8">
+                    <h3>Select Today’s Exercise</h3>
+                    <div class="row g-4">
+                        {
+                            cards.map(card => <Card
+                                key={card.id}
+                                card={card}
+                            ></Card>)
+                        }
+                    </div>     
+                </div>
+                <div className="col-md-4">
+                    <Sidebar></Sidebar>
+                </div>
+            </div>
+            
         </div>
     );
 };
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div class="row">
+            <div className="com-md-12">
+                <div className="personal-details">
+                    <h4 className="name">Adnan Khan</h4>
+                    <div className="physical-info">
+                        <h4>64</h4>
+                        <h4>5.4</h4>
+                        <h4>30</h4>
+                    </div>
+                    <div className="physical-info">
+                        <p>Weight</p>
+                        <p>Height</p>
+                        <p>Age</p>
+                    </div>
+                </div>
+                <div className="break">
+                    <h4>Add a Break</h4>
+                    <div className="break-info">
+                        <p>10s</p>
+                        <p>20s</p>
+                        <p>30s</p>
+                        <p>40s</p>
+                        <p>50s</p>
+                        <p>60s</p>
+                    </div>
+                </div>
+                <div className="exercise-info">
+                    <h4>Exercise Details</h4>
+                    <p><b>Exercise Time:</b></p>
+                    <p><b>Break Time:</b></p>
+                </div>
+                <button className="btnm btn-warning activity">Activity Completed</button>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
